03cd891eafa52a86c32ceb7484f38a3dcfd2422d
Your Name <your.email@example.com>
2014-10-04 02:11:47 +0000

    Something ruby fails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'd1edda48dea04ddfc1c6724342a2a0a1bfbafde916ebb9d440b15f74da46568131baf31fed09fb17378c40366193945a7ad8dc8d295ba2b9b5bfe2848ed79ba2'
